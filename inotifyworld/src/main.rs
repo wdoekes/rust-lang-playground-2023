17f4005b1b3c1b7669c28cc394c820f7377d9e2a
Walter Doekes <walter+github@wjd.nu>
2023-05-04 21:53:13 +0200

    inotifyworld: Playing with error propagation

--- inotifyworld/src/main.rs
+++ inotifyworld/src/main.rs
@@ -1,4 +1,11 @@
-use std::env;
+use std::{
+    env,
+    error,
+    //io,
+    fs,
+    path,
+    //str,
+};
 
 use inotify::{
     EventMask,
@@ -6,17 +13,46 @@ use inotify::{
     WatchMask,
 };
 
+// If we return "Result<(), io::Error>" we cannot handle other errors.
+// If we instead return "Result<(), Box<dyn error::Error>>" we can
+// return a multitude of errors automatically. Now both
+// "metadata.modified()?" and "...elapsed()?" can have the
+// error-propagating-question mark.
+fn do_traverse(root_dir: &path::PathBuf) -> Result<(), Box<dyn error::Error>> {
+    println!("Entries modified in the last 24 hours in {:?}:", root_dir);
 
-fn main() {
-    let mut inotify = Inotify::init()
-        .expect("Failed to initialize inotify");
+    for entry in fs::read_dir(root_dir)? {
+        let entry = entry?;
+        let path = entry.path();
 
-    let current_dir = env::current_dir()
-        .expect("Failed to determine current directory");
+        let metadata = fs::metadata(&path)?;
+        let last_modified = metadata.modified()?.elapsed()?.as_secs();
+
+        if last_modified < 24 * 3600 && metadata.is_file() {
+            println!(
+                "Last modified: {:?} seconds, is read only: {:?}, size: {:?} bytes, filename: {:?}",
+                last_modified,
+                metadata.permissions().readonly(),
+                metadata.len(),
+                path.file_name().ok_or("No filename")
+            );
+        }
+    }
+    Ok(())
+}
+
+fn do_some_error() -> Result<(), Box<dyn error::Error>> {
+    //str::from_utf8(b"\x81")?; // INVALID!
+    //return Err(Box::new(io::Error::new(io::ErrorKind::Other, "Something went wrong")));
+    Ok(())
+}
+
+fn do_inotify(root_dir: &path::PathBuf) -> Result<(), Box<dyn error::Error>> {
+    let mut inotify = Inotify::init()?;
 
     inotify
         .add_watch(
-            current_dir,
+            root_dir,
             WatchMask::MODIFY | WatchMask::CREATE | WatchMask::DELETE,
         )
         .expect("Failed to add inotify watch");
@@ -50,5 +86,27 @@ fn main() {
                 }
             }
         }
+    }
+    //Ok(()) // unreachable for now
+}
+
+fn run() -> Result<(), Box<dyn error::Error>> {
+    let args: Vec<String> = env::args().collect();
+
+    let root_dir: path::PathBuf;
+    match args.len() {
+        1 => { root_dir = env::current_dir().expect("No current dir"); },
+        _ => { root_dir = path::PathBuf::from(&args[1]); },
+    }
+    do_some_error()?;
+    do_traverse(&root_dir)?;
+    do_inotify(&root_dir)?;
+    Ok(())
+}
+
+fn main() {
+    match run() {
+        Ok(_) => {},
+        Err(msg) => { eprintln!("Error: {msg}"); std::process::exit(1); },
     }
 }
